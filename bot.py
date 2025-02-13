08d6a575fa035cb5ae30203e03b8848cd66b1351
nguyenscg <nguyencsg@gmail.com>
2025-02-13 16:22:54 -0600

    setup virtual environment variables and setup bot

--- bot.py
+++ bot.py
@@ -1,2 +1,21 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env
+load_dotenv()
+
+# get token from env
+TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL_ID = 1339716688748216392
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Mingyu Bot just logged in!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send(f"Yo! Mingyu bot just logged in.")
+
+bot.run(TOKEN)
